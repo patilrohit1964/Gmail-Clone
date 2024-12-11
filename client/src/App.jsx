97366bd92563ee6a88233c92efe7d0fa5f16aedf
patilrohit1964 <patilrohit19649@gmail.com>
2024-12-11 23:27:38 +0530

    added styles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,9 +5,13 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Login from './components/Login'
 import SignUp from './components/SignUp'
 import { Dashboard } from './components/Dashboard'
-
+import "./components/style.css"
 function App() {
   const myRouter = createBrowserRouter([
+    {
+      path: "/",
+      Component: SignUp
+    },
     {
       path: "login",
       Component: Login
@@ -29,3 +33,15 @@ function App() {
 }
 
 export default App
+
+
+/*
+
+colors we use in pr
+1.f5f7fb bg-color
+2.#ffffff box color
+3.#33394c text color
+4.4e7cff primary 1
+5.#7033ff primary 2
+6.#f65164 primary 3
+*/
